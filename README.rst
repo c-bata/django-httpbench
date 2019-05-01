4877e4a344f09d60f2974168e017da9b13393dc1
c-bata <contact@c-bata.link>
2019-05-01 15:06:53 +0900

    Add httpbench package

--- README.rst
+++ README.rst
@@ -1,2 +1,5 @@
-# django-httpbench
+django-httpbench
+================
+
 HTTP load testing tools for Django. Like an apache bench but requests are sent from authenticated users.
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-httpbench',
+    version='0.0.1',
+    packages=['httpbench'],
+    description='HTTP load testing tools for Django. Like an apache bench '
+                'but requests are sent from authenticated users.',
+    long_description=README,
+    author='Masashi Shibata',
+    author_email='contact@c-bata.link',
+    url='https://github.com/c-bata/django-httpbench',
+    license='MIT',
+    install_requires=[
+        'Django',
+    ]
+)
