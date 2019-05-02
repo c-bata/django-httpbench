eaf542ffdc83c02af694d7f684949c06c4a02d18
c-bata <contact@c-bata.link>
2019-05-02 16:47:44 +0900

    Avoid uusing deprecated middleware hook point

--- httpbench/middlewares.py
+++ httpbench/middlewares.py
@@ -1,13 +1,16 @@
 from django.contrib import auth
-from django.utils.deprecation import MiddlewareMixin
 
 from .header import get_username_wsgi_env_key
 
 
-class HeaderAuthMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+class HeaderAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         username = request.META.get(get_username_wsgi_env_key())
         user = auth.authenticate(request, username=username)
         if user:
             request.user = user
             auth.login(request, user)
+        return self.get_response(request)
