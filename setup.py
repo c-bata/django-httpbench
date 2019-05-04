e5dab7104e0ef0982e5dfc1c9d0f5c1365cab4d6
c-bata <contact@c-bata.link>
2019-05-05 00:35:11 +0900

    Bump up to v0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='djangohttpbench',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(),
     description='HTTP load testing tools for Django. Like an apache bench '
                 'but requests are sent from authenticated users.',
